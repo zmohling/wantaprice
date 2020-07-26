b7c3dd66c55b0b2a653e38ddc0313cd043b2a3bb
Zachary Mohling <zmohling@gmail.com>
2020-07-26 17:55:23 -0600

    Change scope of model fields

--- services/identity/go.mod
+++ services/identity/go.mod
@@ -1,3 +1,8 @@
 module github.com/zmohling/wantaprice/services/identity
 
 go 1.14
+
+require (
+	github.com/google/uuid v1.1.1
+	github.com/zmohling/wantaprice/pkg/model v0.0.0-20200726233651-07d2f6269328
+)
--- services/identity/service.go
+++ services/identity/service.go
@@ -1,7 +1,44 @@
 package main
 
-import "github.com/zmohling/wantaprice/pkg/model"
+import (
+	"database/sql"
+	"log"
 
-type Service interface {
-	CreateUser(user model.User)
+	"github.com/google/uuid"
+
+	"github.com/zmohling/wantaprice/pkg/model"
+)
+
+// IdentityService ...
+type IdentityService interface {
+	CreateUser(profile model.Profile, password string) (model.User, error)
+}
+
+type identityService struct {
+	db sql.DB
+}
+
+func (s *identityService) CreateUser(profile model.Profile, password string) (model.User, error) {
+	tx, err := s.db.Begin()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tx.Rollback()
+
+	insertUserStmt, err := tx.Prepare("INSERT INTO `users` VALUES (1, 2, 3, 4)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer insertUserStmt.Close()
+
+	primaryKey, _ := uuid.NewUUID()
+	user := model.User{}
+
+	insertUserStmt.Exec()
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
